0d1eac4b02ce7222c3604786f746e17d4654490f
LeonKHart <1005945503@u.icesi.edu.co>
2023-04-22 22:46:19 -0400

    ...

--- src/model/Enterprise.java
+++ src/model/Enterprise.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.InputMismatchException;
-//import java.util.Scanner;
 import java.util.Scanner;
 
 public class Enterprise {
